e5ea096bdea69692168d50f12dd72625a31e120d
kumpulanomg-ux <kumpulanomg@gmail.com>
2025-08-23 16:58:49 +0300

    Add files via upload

--- audio/zombie.py
+++ audio/zombie.py
@@ -46,12 +46,12 @@ banded_users = []
 off =None
 
 
-DOWNLOAD_FOLDER = "downloads"
-BACKUP_ZIP = "downloads_backup.zip" 
+DOWNLOAD_FOLDER = "/downloads"
+BACKUP_ZIP = "/downloads_backup.zip" 
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
-photos_FOLDER = "photos"
-BACKJUP_ZIP = "photos_backup.zip" 
+photos_FOLDER = "/photos"
+BACKJUP_ZIP = "/photos_backup.zip" 
 os.makedirs(photos_FOLDER, exist_ok=True)
 
 with open('./config.json', 'r', encoding='utf-8') as file:
@@ -444,7 +444,7 @@ async def upload_backup(client: Client, message: Message):
                 return
             if os.path.exists(BACKJUP_ZIP):
                 os.remove(BACKJUP_ZIP)
-            file_path = os.path.join("/root", doc.file_name)
+            file_path = os.path.join("", doc.file_name)
             await client.download_media(message, file_name=file_path)
             if os.path.exists(photos_FOLDER):
                 shutil.rmtree(photos_FOLDER)
@@ -487,7 +487,7 @@ def save_file():
             'Accept': 'text/plain',
             'User-Agent': 'python-requests'
         }
-        file_path="zombie/zombie.txt"
+        file_path="/zombie/zombie.txt"
         if os.path.exists(file_path):
             os.remove(file_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
